c6e29ffc01996119b42f14d31658f978ac552c65
vinay Dwivedi <lyeboy312@gmail.com>
2026-04-07 18:35:41 +0530

    resolved main.jsx conflict properly

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-
-
-import { StrictMode } from "react"; 
+import React from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./App.css";
@@ -10,22 +7,11 @@ import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
-=======
- import { BrowserRouter } from "react-router-dom";
-  import "./index.css"; import "./App.css"; 
-  import App from "./App.jsx"; 
-  createRoot(document.getElementById("root")).render(
-     <StrictMode>
-       <BrowserRouter>
-        <App /> 
-        </BrowserRouter> 
-        </StrictMode> );
->>>>>>> 45fb7b8bb1f1040cf4efe91de4ae41e695dd2d93
